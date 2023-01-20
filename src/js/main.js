859d1404bcae078906f91ac9aded68050293f55d
bacugniano <bakuntsev@gmail.com>
2023-01-20 20:45:23 +0300

    update html (add menu, burger and footer), style and JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,32 @@
 'use script';
 
-let burger = document.querySelector('.nav__burger');
-let menu = document.querySelector('.nav__list');
+const burger = document.querySelector('.nav__burger');
+const nav = document.querySelector('.nav');
+const menu = document.querySelector('.nav__list');
+const links = document.querySelectorAll('.nav__link');
+const body = document.querySelector('body');
 
+// открытие/закрытие меню
 burger.onclick = () => {
-
   menu.classList.toggle('nav__list--active');
-  burger.classList.toggle('is-active');   
-
+  burger.classList.toggle('is-active');  
+  body.classList.toggle('active');  
 }
 
-// $('.nav__link').click(function(){
-//   $('.nav__burger').removeClass("is-active");
-//   $('.nav__list').removeClass('nav__list--active');
-//   $('body').removeClass('body--active');
-// });
+// закрытие меню при клике на ссылку
+links.forEach((el) => el.addEventListener('click', function() {
+  if (el.classList.contains('nav__link')) {
+    menu.classList.remove('nav__list--active');
+    burger.classList.remove('is-active'); 
+    body.classList.remove('active');
+  }
+}));
+
+// закрытие меню при клике на пустой области
+document.addEventListener('click', (e) => {
+  if (! nav.contains(e.target)) {
+    menu.classList.remove('nav__list--active');
+    burger.classList.remove('is-active'); 
+    body.classList.remove('active');
+  }
+});
