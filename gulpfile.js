ee5e7b4d2bc3872ff3128f2d5e4919cd460c531a
bacugniano <bakuntsev@gmail.com>
2023-01-20 20:26:14 +0300

    update gulpfile (svg, imagemin)

--- gulpfile.js
+++ gulpfile.js
@@ -1,146 +1,176 @@
-// Импорт пакетов
 const gulp = require('gulp')
-const less = require('gulp-less')
-const stylus = require('gulp-stylus')
+const del = require('del')
 const sass = require('gulp-sass')(require('sass'))
 const rename = require('gulp-rename')
-const cleanCSS = require('gulp-clean-css')
-const ts = require('gulp-typescript')
-//const coffee = require('gulp-coffee')
+const minCSS = require('gulp-clean-css')
 const babel = require('gulp-babel')
 const uglify = require('gulp-uglify')
-const concat = require('gulp-concat')
 const sourcemaps = require('gulp-sourcemaps')
-const autoprefixer = require('gulp-autoprefixer')
+const autoprefix = require('gulp-autoprefixer')
 const imagemin = require('gulp-imagemin')
 const htmlmin = require('gulp-htmlmin')
-const size = require('gulp-size')
-//const gulppug = require('gulp-pug')
 const newer = require('gulp-newer')
-const browsersync = require('browser-sync').create()
-const del = require('del')
+const browserSync = require('browser-sync').create()
+const svgSprite = require('gulp-svg-sprite')
+const woff = require('gulp-ttf2woff')
+const woff2 = require('gulp-ttf2woff2')
 
-// Пути исходных файлов src и пути к результирующим файлам dest
-const paths = {
+//пути к файлам
+const path = {
   html: {
-    src: ['src/*.html', 'src/*.pug'],
+    src: 'src/*.html',
     dest: 'dist/'
   },
   styles: {
-    src: ['src/styles/**/*.sass', 'src/styles/**/*.scss', 'src/styles/**/*.styl', 'src/styles/**/*.less', 'src/styles/**/*.css'],
+    src: 'src/styles/scss/**/*.scss',
     dest: 'dist/css/'
   },
-  scripts: {
-    src: ['src/scripts/**/*.coffee', 'src/scripts/**/*.ts', 'src/scripts/**/*.js'],
+  js: {
+    src: 'src/js/*.js',
     dest: 'dist/js/'
   },
   images: {
     src: 'src/img/**',
     dest: 'dist/img/'
+  },
+  svg: {
+    src: 'src/img/svg/*.svg',
+    dest: 'dist/img/'
+  },
+  fonts: {
+    src: 'src/fonts/*.ttf',
+    dest: 'dist/fonts/'
   }
 }
 
-// Очистить каталог dist, удалить все кроме изображений
-function clean() {
-  return del(['dist/*', '!dist/img'])
+//очистка папки dist
+const clean = () => del(['dist/*', '!dist/img', '!dist/fonts'])
+
+//таск для sass
+const styles = () => {
+  return gulp.src(path.styles.src)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefix({
+      cascade: false
+    }))
+    .pipe(minCSS())
+    .pipe(rename({
+      basename: 'style',
+      suffix: '.min'
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(path.styles.dest))
+    .pipe(browserSync.stream())
 }
 
-// Обработка html и pug
-function html() {
-  return gulp.src(paths.html.src)
-  //.pipe(gulppug())
-  .pipe(htmlmin({ collapseWhitespace: true }))
-  .pipe(size({
-    showFiles:true
-  }))
-  .pipe(gulp.dest(paths.html.dest))
-  .pipe(browsersync.stream())
+//таск для скриптов
+const scripts = () => {
+  return gulp.src(path.js.src)
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(uglify())
+    .pipe(rename({
+      basename: 'main',
+      suffix: '.min'
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(path.js.dest))
+    .pipe(browserSync.stream())
 }
 
-// Обработка препроцессоров стилей
-function styles() {
-  return gulp.src(paths.styles.src)
-  .pipe(sourcemaps.init())
-  //.pipe(less())
-  //.pipe(stylus())
-  .pipe(sass().on('error', sass.logError))
-  .pipe(autoprefixer({
-    cascade: false
-  }))
-  .pipe(cleanCSS({
-    level: 2
-  }))
-  .pipe(rename({
-    basename: 'style',
-    suffix: '.min'
-  }))
-  .pipe(sourcemaps.write('.'))
-  .pipe(size({
-    showFiles:true
-  }))
-  .pipe(gulp.dest(paths.styles.dest))
-  .pipe(browsersync.stream())
+//таск для сжатия изображений
+const img = () => {
+  return gulp.src(path.images.src)
+    .pipe(newer(path.images.dest))
+    .pipe(imagemin([
+      imagemin.mozjpeg({
+        quality: 80,
+        progressive: true
+      }),
+      imagemin.optipng({
+        optimizationLevel: 2
+      }),
+    ]))
+    .pipe(gulp.dest(path.images.dest))
 }
 
-// Обработка Java Script, Type Script и Coffee Script
-function scripts() {
-  return gulp.src(paths.scripts.src)
-  .pipe(sourcemaps.init())
-  //.pipe(coffee({bare: true}))
-  /*
-  .pipe(ts({
-    noImplicitAny: true,
-    outFile: 'main.min.js'
-  }))
-  */
-  .pipe(babel({
-    presets: ['@babel/env']
-  }))
-  .pipe(uglify())
-  .pipe(concat('main.min.js'))
-  .pipe(sourcemaps.write('.'))
-  .pipe(size({
-    showFiles:true
-  }))
-  .pipe(gulp.dest(paths.scripts.dest))
-  .pipe(browsersync.stream())
+//таск для сжатия html
+const html = () => {
+  return gulp.src(path.html.src)
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest(path.html.dest))
+    .pipe(browserSync.stream())
 }
 
-// Сжатие изображений
-function img() {
-  return gulp.src(paths.images.src)
-  .pipe(newer(paths.images.dest))
-  .pipe(imagemin({
-    progressive: true
-  }))
-  .pipe(size({
-    showFiles:true
-  }))
-  .pipe(gulp.dest(paths.images.dest))
+//svg sprite
+const svg = () => {
+  return gulp.src(path.svg.src)
+    .pipe(svgSprite({
+      mode: {
+        symbol: {
+          dest: '.',
+          sprite: 'sprite.svg'
+        },
+      },
+      shape: {
+        transform: [
+          {svgo: {
+            plugins: [
+              {
+                name: "removeAttrs",
+                params: {
+                  attrs: "(fill|stroke)"
+                }
+              }
+            ]
+          }}
+        ],
+      },
+    }))
+    .pipe(gulp.dest(path.svg.dest))
 }
 
-// Отслеживание изменений в файлах и запуск лайв сервера
-function watch() {
-  browsersync.init({
+
+//конвертирование шрифтов
+// to woff
+
+const toWoff = () => {
+  return gulp.src(path.fonts.src)
+    .pipe(newer(path.fonts.dest))
+    .pipe(woff())
+    .pipe(gulp.dest(path.fonts.dest))
+}
+
+//to woff2
+const toWoff2 = () => {
+  return gulp.src(path.fonts.src)
+    .pipe(newer(path.fonts.dest))
+    .pipe(woff2())
+    .pipe(gulp.dest(path.fonts.dest))
+}
+
+//вотчер
+const watch = () => {
+  browserSync.init({
     server: {
-        baseDir: "./dist"
+      baseDir: "./dist"
     }
   })
-  gulp.watch(paths.html.dest).on('change', browsersync.reload)
-  gulp.watch(paths.html.src, html)
-  gulp.watch(paths.styles.src, styles)
-  gulp.watch(paths.scripts.src, scripts)
-  gulp.watch(paths.images.src, img)
+
+  gulp.watch(path.html.dest).on('change', browserSync.reload)
+  gulp.watch(path.html.src, html)
+  gulp.watch(path.styles.src, styles)
+  gulp.watch(path.js.src, scripts)
+  gulp.watch(path.images.src, img)
+  gulp.watch(path.svg.src, gulp.parallel(svg))
+  gulp.watch(path.fonts.src, gulp.parallel(toWoff, toWoff2))
 }
 
-// Таски для ручного запуска с помощью gulp clean, gulp html и т.д.
-exports.clean = clean
+exports.svg = svg
 
-exports.html = html
-exports.styles = styles
-exports.scripts = scripts
-exports.img = img
-exports.watch = watch
-
-// Таск, который выполняется по команде gulp
-exports.default = gulp.series(clean, html, gulp.parallel(styles, scripts, img), watch)
+exports.default = gulp.series(clean, html, gulp.parallel(styles, scripts, img, svg, toWoff, toWoff2), watch)
